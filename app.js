a77a767a38e3e1645e1cde91ff4e798e9a3978b9
genesisbell <bellmon90@hotmail.com>
2020-08-14 13:29:40 -0700

    Fix google redirect

--- app.js
+++ app.js
@@ -72,11 +72,10 @@ passport.deserializeUser(function(id, done) {
     });
 });
 
-////////
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/todolist",
+    callbackURL: "https://damp-ocean-76119.herokuapp.com/auth/google/todolist",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
